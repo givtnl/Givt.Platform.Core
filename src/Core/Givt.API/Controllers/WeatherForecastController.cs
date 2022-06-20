d98db353e3aef8a748eb8ba4f3946a86980b02fe
Phalox <toonpt@gmail.com>
2022-06-20 16:43:22 +0200

    Demo material

--- src/Core/Givt.API/Controllers/WeatherForecastController.cs
+++ src/Core/Givt.API/Controllers/WeatherForecastController.cs
@@ -25,7 +25,8 @@ namespace Givt.API.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = "Toon was here"
+                //Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
